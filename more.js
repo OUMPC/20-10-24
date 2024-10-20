d3008e23e6897821fe294c447cddb722449fa8d4
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-10-20 17:03:32 +0700

    update meta

--- more.js
+++ more.js
@@ -14,9 +14,9 @@ document.body.onclick = () => {
     }
 }
 
-// setTimeout(() => {
-//     alert('Hôm nay là một ngày vô cùng đặc biệt - ngày 20/10, ngày Phụ nữ Việt Nam! Nhân dịp này, CLB Lập trình trên thiết bị di động xin gửi những lời chúc tốt đẹp nhất đến tất cả các bạn nữ và các cô giảng viên. Chúc các bạn luôn thành công trong sự nghiệp, hạnh phúc trong tình yêu và luôn tỏa sáng như những đóa hoa rực rỡ. ❤❤');
-// }, 8000);
+setTimeout(() => {
+    alert('Hôm nay là một ngày vô cùng đặc biệt - ngày 20/10, ngày Phụ nữ Việt Nam! Nhân dịp này, CLB Lập trình trên thiết bị di động xin gửi những lời chúc tốt đẹp nhất đến tất cả các bạn nữ và các cô giảng viên. Chúc các bạn luôn thành công trong sự nghiệp, hạnh phúc trong tình yêu và luôn tỏa sáng như những đóa hoa rực rỡ. ❤❤');
+}, 8000);
 
 document.body.onloadedmetadata = () => {
     music.play();
